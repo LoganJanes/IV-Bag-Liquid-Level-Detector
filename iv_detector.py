82c9e142637fa53695e90a867636910471a8a79a
LoganJanes <lmjanes@mun.ca>
2025-07-18 09:28:42 -0230

    Grayscale
    Added implementation to Grayscale the image.

--- iv_detector.py
+++ iv_detector.py
@@ -9,3 +9,14 @@ class IVBagLevelDetector:
     def __init__(self, debug=False):
         self.debug = debug
         self.processing_steps = {}
+
+#Our first step is Grayscaling the image to remove it's colour with the following function:
+
+def step1_grayscale(self, image: np.ndarray) -> np.ndarray:
+    if len(image.shape) == 3:
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray_image = image.copy()
+    if self.debug:
+        self.processing_steps['grayscale'] = gray_image
+    return gray_image
